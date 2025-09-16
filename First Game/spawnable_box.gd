fdb27e82c289edcd51a90e2d6dab4d1c457e9c95
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-09-16 12:27:29 -0700

    block solidify waits until valid spot

--- First Game/spawnable_box.gd
+++ First Game/spawnable_box.gd
@@ -1,20 +1,25 @@
 extends StaticBody2D
 
-@export var grow_duration: float = 0.1
+@export var grow_duration: float = 0.2
 @onready var player = $"../Player"
 @onready var collision_shape_2d = $CollisionShape2D
 
 @onready var solid_block = $SolidBlock
 @onready var ghost_block = $GhostBlock
 
+@onready var area_2d = $Area2D
 
-var spring_duration: float = 2.0
+var spring_duration: float = 2.5
+var overshoot_factor: float = 1.3
 
 var target_position: Vector2
 
 var is_solid: bool = true
 
 var is_first_spring: bool = true
+
+
+var trying_to_solidify: bool = false
 
 func _ready():
 	scale = Vector2(1, 0.001)
@@ -28,10 +33,7 @@ func _physics_process(delta):
 			is_solid = false
 			target_position = player.global_position
 		else:
-			active_tween.kill()
-			is_solid = true
-			target_position = global_position
-			global_position = target_position
+			block_spot_check_valid()
 	
 	if !is_solid && is_first_spring:
 		is_first_spring = false
@@ -50,7 +52,7 @@ func spring_to_target():
 	if active_tween:
 		active_tween.kill()
 	active_tween = create_tween()
-	var overshoot_factor: float = 1.3
+	
 	var start_position = global_position
 	var final_position = target_position
 	var overshoot_position = start_position.lerp(final_position, overshoot_factor)
@@ -61,3 +63,12 @@ func spring_to_target():
 	# Part 2: Move from the overshoot back to the final position.
 	active_tween.tween_property(self, "global_position", final_position, spring_duration * 0.3)\
 			   .set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
+
+
+func block_spot_check_valid(): # wait until there are no overlapping area2d's, then kill tween and set to solid etc.
+	while area_2d.get_overlapping_bodies().size() > 0:
+		await get_tree().process_frame
+	active_tween.kill()
+	is_solid = true
+	target_position = global_position
+	global_position = target_position
