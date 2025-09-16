17ae8e7143e2a124c48476c62be2f88de7679c0f
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-09-16 13:18:33 -0700

    Coyote time added

---
+++ First Game/scripts/Global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+#func _ready():
+	#Engine.time_scale = 0.1
--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -5,12 +5,15 @@ const SPAWN_OFFSET_Y = 0.0
 
 const SPEED = 80.0
 const JUMP_VELOCITY = -190.0
+var was_on_floor: bool = false
 
 var can_spawn_box: bool = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var area_2d = $Area2D
+@onready var coyote_timer = $CoyoteTimer
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("spawn_box") && can_spawn_box:
@@ -20,10 +23,17 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
+	if !custom_on_ground() && was_on_floor:
+		coyote_timer.start()
+	
+	print(coyote_timer.time_left)
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (custom_on_ground() || !coyote_timer.is_stopped()):
 		velocity.y = JUMP_VELOCITY
+		coyote_timer.stop()
+		was_on_floor = false
 
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -35,7 +45,7 @@ func _physics_process(delta):
 		animated_sprite.flip_h = true
 	
 	# Play animations
-	if is_on_floor():
+	if custom_on_ground():
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
@@ -48,12 +58,16 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	move_and_slide()
-
+	was_on_floor = custom_on_ground()
 
 func spawn_box():
-	if is_on_floor():
+	if custom_on_ground():
 		var box_instance = box_scene.instantiate()
 		box_instance.global_position = self.global_position + Vector2(0, SPAWN_OFFSET_Y)
 		get_parent().add_child(box_instance)
 		#position.y -= 16.0
+
+func custom_on_ground() -> bool:
+	return (area_2d.get_overlapping_bodies().size() > 1) || is_on_floor()
